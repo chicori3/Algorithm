bcdf5caa43b7cf3bca185bfbe036409861a5d61b
I C-AN <chicori3@naver.com>
2022-06-03 16:36:33 +0900

    [Silver II] Title: 유기농 배추, Time: 156 ms, Memory: 15752 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static int M;
+    static int N;
+    static int K;
+    static int[][] map;
+    static boolean[][] visited;
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            result = 0;
+            map = new int[M][N];
+            visited = new boolean[M][N];
+
+            for (int j = 0; j < K; j++) {
+                st = new StringTokenizer(br.readLine());
+                map[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
+            }
+
+            for (int j = 0; j < M; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (map[j][k] == 1 && !visited[j][k]) {
+                        result++;
+                        dfs(j, k);
+                    }
+                }
+            }
+            System.out.println(result);
+        }
+    }
+
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < M && ny < N) {
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+}
