109d21b9230e744a7f6085ecc505430e6c5262c0
이시안 <chicori3@naver.com>
2022-07-22 15:22:34 +0900

    Time: 236 ms (72.79%), Space: 0B (100.00%) - LeetHub

--- 627-swap-salary/627-swap-salary.sql
+++ 627-swap-salary/627-swap-salary.sql
@@ -1,11 +1,4 @@
 # Write your MySQL query statement below
-# select id,name, 
-# case sex
-#     when 'm' then 'f'
-#     else 'f' end,
-# salary
-# from salary
-
 update salary
 set sex = (
     case sex
