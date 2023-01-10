f9c10be88b0454ec42aa792e29602dc0176afdcc
이시안 <chicori3@naver.com>
2023-01-10 13:15:54 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int price) {
+        if (price >= 500000) {
+            return (int) (price - price * 0.2);
+        }
+        if (price >= 300000) {
+            return (int) (price - price * 0.1);
+        }
+        if (price >= 100000) {
+            return (int) (price - price * 0.05);
+        }
+
+        return price;
+    }
+}
